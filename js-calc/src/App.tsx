7e4e0c0ade6f7fbae6ab5d262f0de30dd8378492
Richard Jean-Baptiste <Richinbk1@gmail.com>
2025-03-04 16:40:51 -0500

    added sumEquations function

--- js-calc/src/App.tsx
+++ js-calc/src/App.tsx
@@ -3,20 +3,116 @@ import './App.css';
 
 function App() {
   const [cVal, setCVal] = useState("0");
+  const [addDecimal, SetAddDecimal] = useState(true);
 
   const clearDisplay = () => {
     setCVal("0");
   }
 
+  const isOperand = (x: string | undefined) : boolean => {
+
+    if(x == undefined){
+      return false;
+    }
+
+    if(x == "/" || x == "+" || x == "-" || x == "*"){
+      return true;
+    } 
+
+    return false;
+  }
+
   const addInput = (input: string) => {
+
+    const lastChar = cVal.substring(cVal.length - 1);
+    let x;
 
     if(cVal == "0"){
       setCVal(input);
-    } else if (cVal.includes(".") && input == ".") {
-      return;
     } else {
-      let x = cVal + input;
+      if(input == "."){
+
+        if(isOperand(lastChar)){
+          return;
+        };
+
+        if(addDecimal){
+          x = cVal + input;
+          SetAddDecimal(false);
+        } else {
+          return;
+        }
+      } else if(input == "+" || input == "-" || input == "*" || input == "/") {
+
+        // if(isOperand(lastChar)){
+        //   return;
+        // }
+
+        x = cVal + input;
+        SetAddDecimal(true);
+
+      } else {
+        x = cVal + input;
+        setCVal(x);
+        return;
+      }
+      
       setCVal(x);
+    }
+  }
+
+  const sumValues = (leftSide: number, operand: string, rightSide: string) => {
+
+    if(operand == "+"){
+      return leftSide + Number(rightSide);
+    } else if(operand == "*"){
+      return leftSide * Number(rightSide);
+    } else if (operand == "/") {
+      return leftSide / Number(rightSide);
+    }
+  }
+
+  const sumEquation = () => {
+    
+    let temp: string | undefined = "";
+    //let tempNegative;
+    let tempOperand = "+";
+    let result: number | undefined = 0;
+    let current;
+    //let leftSide: int | null = null;
+    //let rightSide: int | null = null;
+
+    const x: string[] = cVal.split("").reverse();
+    const xLength = x.length;
+    console.log(x);
+
+    for(let i = 0; i < xLength; i++){
+      current = x.pop();
+
+      if(current == undefined){
+        break;
+      }
+      
+      if(!isOperand(current)){
+        temp += current;
+      } else {        
+        // character is operand
+
+        if(result == undefined || temp == undefined){
+          break;
+        }
+
+        console.log(result + " " + tempOperand + " " + temp);
+        result = sumValues(result, tempOperand, temp);
+
+        // Switch to the next operand
+        tempOperand = current;
+        temp = "";
+      }
+    }
+
+    if( result != undefined) {
+      console.log(sumValues(result,tempOperand, temp));
     }
     
   }
@@ -52,7 +148,7 @@ function App() {
       </div>
 
       <div>
-        <button id="equals">=</button>
+        <button id="equals" onClick={sumEquation}>=</button>
         <button id="clear" onClick={clearDisplay}>clear</button>
       </div>
       
